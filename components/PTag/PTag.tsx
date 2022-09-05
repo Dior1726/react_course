daec4f24c03cb5f42a69d9087e7d28fa616067f8
Dior17 <muxammad_909@mail.ru>
2022-09-05 10:27:40 +0600

    create main components

--- components/PTag/PTag.tsx
+++ components/PTag/PTag.tsx
@@ -2,10 +2,10 @@ import { PTagProps } from './PTag.props'
 import styles from './PTag.module.css'
 import cn from 'classnames'
 
-export const PTag = ({children, size = 'm'}: PTagProps): JSX.Element => {
+export const PTag = ({children, size = 'm', className, ...props}: PTagProps): JSX.Element => {
   return (
     <p
-      className={cn({
+      className={cn( className,  {
         [styles.s]: size == 's',
         [styles.m]: size == 'm',
         [styles.l]: size == 'l',
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
-import { Button, HTag, PTag } from '../components'
+import { Button, HTag, PTag, Tag } from '../components'
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +9,10 @@ const Home: NextPage = () => {
       <Button appearance='primary'>Button</Button>
       <Button arrow='right' appearance='ghost'>Button</Button>
       <PTag size='m'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi eveniet accusantium placeat cupiditate cum sed consequatur sequi distinctio delectus, velit nulla veritatis id maiores laboriosam quia! Alias nesciunt non voluptas. </PTag>
+      <Tag color='grey' size='m' >Hello</Tag>
+      <Tag color='green' size='s' >Hello</Tag>
+      <Tag color='ghost' size='m' >Hello</Tag>
+      <Tag color='red' size='s' >Hello</Tag>
     </div>
   )
 }
